1f133893e73b1eceb0df28f27647f728c93e9e59
FAteme <miss.l3lue@gmail.com>
2021-11-16 15:23:48 +0330

    created grids

---
+++ script.js
@@ -0,0 +1,20 @@
+let gridColumns = '64';
+gridItems = gridColumns * gridColumns;
+
+const board = document.querySelector('.board');
+board.style.gridTemplateColumns = `repeat(${gridColumns} , 1fr)`;
+function creatDiv(gridItems) {
+    for ( i =0 ; i < gridItems ; i++) {
+        let newDiv = document.createElement('div');
+        board.appendChild(newDiv);
+    }
+}
+
+creatDiv(gridItems);
+
+const newDivs = Array.from(document.querySelectorAll('.board>div'));
+console.log(newDivs)
+
+newDivs.forEach(div => div.addEventListener('mouseover', function(e) {
+    div.style.backgroundColor = '#582F0E'
+}));
