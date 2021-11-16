e66caa8f711d1b03c7db6d1b3c459fa18e969e7f
FAteme <miss.l3lue@gmail.com>
2021-11-16 18:17:12 +0330

    added slider

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-let gridColumns = '64';
-gridItems = gridColumns * gridColumns;
-
+let gridColumns;
+let gridItems;
 const board = document.querySelector('.board');
-board.style.gridTemplateColumns = `repeat(${gridColumns} , 1fr)`;
-function creatDiv(gridItems) {
+
+function createDiv(gridColumns) {
+    gridItems = gridColumns * gridColumns;
     for ( i =0 ; i < gridItems ; i++) {
         let newDiv = document.createElement('div');
         board.appendChild(newDiv);
     }
-}
-
-creatDiv(gridItems);
-
-const newDivs = Array.from(document.querySelectorAll('.board>div'));
+    board.style.gridTemplateColumns = `repeat(${gridColumns} , 1fr)`;
+    newDivs = Array.from(document.querySelectorAll('.board>div'));
 console.log(newDivs)
 
 newDivs.forEach(div => div.addEventListener('mouseover', function(e) {
     div.style.backgroundColor = '#582F0E'
 }));
+}
+
+createDiv('32');
+
+function removeDivs () {
+    newDivs = Array.from(document.querySelectorAll('.board>div'));
+    newDivs.forEach(div => div.remove())
+}
+
+
+
+
+
+
+// SLIDER SETTINGS
+const slider = document.getElementById('grid-slider')
+const sliderSpan = document.getElementById('slider-span')
+sliderSpan.textContent = slider.value + "*" + slider.value;
+slider.oninput = function() {
+    removeDivs();
+    sliderSpan.textContent = slider.value + "*" + slider.value;
+    gridColumns = slider.value;
+    createDiv(gridColumns);
+}
+
+
+
